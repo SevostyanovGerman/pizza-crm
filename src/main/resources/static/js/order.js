dd9a7d8a468ee53f2dc747f3003056ce836bdd5f
cutcell <ifield88@gmail.com>
2018-02-16 21:19:16 +0300

    add quantity control

--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -1,23 +1,6 @@
 var host = window.location.origin;
-var csrfToken = $("meta[name='_csrf']").attr("content");
-var csrfHeader = $("meta[name='_csrf_header']").attr("content");
-
-function makeRow(name, price) {
-    var markup = "<tr><td>" + name + "</td><td>" + price + "</td></tr>";
-    $("#result").append(markup)
-}
-
-function setCashierTable(name, price) {
-    makeRow(name, price);
-    sum(price)
-}
-
-function sum(data) {
-    var price = parseInt($("#sum").text());
-    var dPrice = parseInt(data);
-    price += dPrice;
-    $("#sum").html(price)
-}
+let csrfToken = $("meta[name='_csrf']").attr("content");
+let csrfHeader = $("meta[name='_csrf_header']").attr("content");
 
 function getProduct(name) {
     $("#backward").removeClass("disable");
@@ -30,11 +13,19 @@ function getProduct(name) {
             xhr.setRequestHeader(csrfHeader, csrfToken);
         },
         success: function (data) {
-            $("#dish").empty();
+            $("#dish").empty().css({"display": "block"});
             $.each(data, function (key, value) {
-                $("#dish").append("<a onclick='setCashierTable(\""+value.name+"\", " + value.price + ")' class=\"middle-panel-white\" href=\"#\"><p>" + value.name + "</p>" + value.price + "</a>")
+                $("#dish").append($([
+                    "<a href='#' class='order-item middle-panel-white'",
+                    "data-item-name=\"" + value.name + "\"",
+                    "data-quantity='1'",
+                    "data-price=" + value.price,
+                    ">",
+                    "<p>" + value.name + "</p>",
+                    "<p>" + value.price + "</p>",
+                    "</a>"
+                ].join("\n")));
             });
-            $("#dish").css({"display": "block"});
         },
         error: function (e) {
         }
@@ -47,3 +38,83 @@ function getCategories() {
     $("#dish").css({"display": "none"});
 }
 
+$(document).ready(function () {
+    $('.order-table').on('click', 'tr', function () {
+        $(this).addClass('highlight').siblings().removeClass('highlight');
+    });
+});
+
+$(document).ready(function () {
+    $('.add-quantity').click(function () {
+        let tr = getSelectedRow();
+        let quantity = parseFloat(tr.find('td:eq(0)').text());
+        quantity++;
+        tr.find('td:eq(0)').text(quantity);
+        updateTotal();
+    });
+});
+
+$(document).ready(function () {
+    $('.subtract-quantity').click(function () {
+        let tr = getSelectedRow();
+        let quantity = parseFloat(tr.find('td:eq(0)').text());
+        if (--quantity <= 0) {
+            tr.remove();
+            updateTotal();
+            return;
+        }
+        tr.find('td:eq(0)').text(quantity);
+        updateTotal();
+    });
+});
+
+$(document).ready(function () {
+    $('.remove-selected-dish').click(function () {
+        let tr = getSelectedRow();
+        tr.prev().addClass('highlight').siblings().removeClass('highlight');
+        updateTotal();
+        tr.remove();
+    });
+});
+
+$(document).ready(function () {
+    $('#dish').on('click', '.order-item', function (e) {
+        e.preventDefault();
+        $('.order-table').append($([
+            "<tr>",
+            "<td>" + $(this).data('quantity') + "</td>",
+            "<td>" + $(this).data('itemName') + "</td>",
+            "<td>" + $(this).data('price') + "</td>",
+        ].join("/n")));
+        updateTotal();
+    });
+});
+
+function getSelectedRow() {
+    let tr = $('.order-table tr.highlight');
+    if (tr.length === 0) {
+        tr = $('.order-table tr:last');
+        tr.addClass('highlight').siblings().removeClass('highlight');
+    }
+    return tr;
+}
+
+function updateTotal() {
+    let total = 0;
+    $('.order-table tr').each(function () {
+        total += getRowTotal($(this));
+    });
+    $('#total').html(total)
+}
+
+function getRowTotal(row) {
+    let quantity = parseFloat(row.find('td:eq(0)').text());
+    if (isNaN(quantity)) {
+        quantity = 0;
+    }
+    let price = parseFloat(row.find('td:eq(2)').text());
+    if (isNaN(price)) {
+        price = 0;
+    }
+    return quantity * price;
+}
