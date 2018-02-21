d279658819de1ea9165e385d4006b7c2e011ad50
Roman Polovincev <polovincev@gmail.com>
2018-02-22 00:52:20 +0300

    add working search product

---
+++ src/main/java/com/pizza/crm/controller/admin/rest/DishControllerRest.java
@@ -0,0 +1,26 @@
+package com.pizza.crm.controller.admin.rest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pizza.crm.model.Dish;
+import com.pizza.crm.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+public class DishControllerRest {
+
+    @Autowired
+    private DishService dishService;
+
+    @RequestMapping(value = "/get/dish")
+    public String productSearchPage() throws JsonProcessingException {
+        Collection<Dish> dish = dishService.getAll();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String dishAsString = objectMapper.writeValueAsString(dish);
+        return dishAsString;
+    }
+}
