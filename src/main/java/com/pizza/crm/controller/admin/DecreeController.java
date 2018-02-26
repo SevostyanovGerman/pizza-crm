adcab570ea060a655f7d7f5c3ae39a60d2614633
dinar <xxxcorsair@yandex.ru>
2018-02-26 19:11:07 +0300

    add Schedule;

--- src/main/java/com/pizza/crm/controller/admin/DecreeController.java
+++ src/main/java/com/pizza/crm/controller/admin/DecreeController.java
@@ -1,14 +1,27 @@
 package com.pizza.crm.controller.admin;
 
+import com.pizza.crm.model.Schedule;
+import com.pizza.crm.service.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DecreeController {
 
+    @Autowired
+    private ScheduleService scheduleService;
+
     @RequestMapping("/newDecree")
     public String newDecree(){
         return "admin/newDecree";
+    }
+
+    @PostMapping("/newDecree/save")
+    public String save(Schedule schedule){
+        scheduleService.save(schedule);
+        return "redirect:/newDecree";
     }
 
 }
