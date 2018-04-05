b503abbe928584c84c06c94fb9c6fb5eca7ae2ee
Igor <igorzaporogtsev@gmail.com>
2018-04-05 11:12:52 +0300

    Create Controller

--- src/main/java/com/pizza/crm/controller/admin/UnitsOfMeasurementController.java
+++ src/main/java/com/pizza/crm/controller/admin/UnitsOfMeasurementController.java
@@ -1,4 +1,26 @@
 package com.pizza.crm.controller.admin;
 
+import com.pizza.crm.service.UnitsOfMeasurmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UnitsOfMeasurementController {
+
+    public final UnitsOfMeasurmentService unitsOfMeasurment;
+
+    @Autowired
+    public UnitsOfMeasurementController(UnitsOfMeasurmentService unitsOfMeasurment) {
+        this.unitsOfMeasurment = unitsOfMeasurment;
+    }
+
+    @RequestMapping("/measurement")
+    public String getAllMeasurement(Model model) {
+        model.addAttribute("allMeasurement", unitsOfMeasurment.getAll());
+        return "UnitsOfMeasurement";
+    }
+
+
 }
