299c803920b4ddf8dae8cc0748780b235692820b
Igor <igorzaporogtsev@gmail.com>
2018-04-05 12:20:51 +0300

    add addUnit Controller

--- src/main/java/com/pizza/crm/controller/admin/UnitsOfMeasurementController.java
+++ src/main/java/com/pizza/crm/controller/admin/UnitsOfMeasurementController.java
@@ -1,9 +1,11 @@
 package com.pizza.crm.controller.admin;
 
+import com.pizza.crm.model.UnitsOfMeasurement;
 import com.pizza.crm.service.UnitsOfMeasurmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,7 +21,14 @@ public class UnitsOfMeasurementController {
     @RequestMapping("/measurement")
     public String getAllMeasurement(Model model) {
         model.addAttribute("allMeasurement", unitsOfMeasurment.getAll());
+        model.addAttribute("AddUnit", new UnitsOfMeasurement());
         return "UnitsOfMeasurement";
+    }
+
+    @RequestMapping(value = "/add")
+    public String addUser(@ModelAttribute("AddUnit") UnitsOfMeasurement unit){
+        unitsOfMeasurment.save(unit);
+        return "redirect:/measurement";
     }
 
 
