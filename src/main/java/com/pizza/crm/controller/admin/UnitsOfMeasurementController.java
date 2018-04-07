1966f226882319706f5a453bb4277f83524e10cd
Igor <igorzaporogtsev@gmail.com>
2018-04-07 09:32:05 +0300

    add Method public String deleteUnit

--- src/main/java/com/pizza/crm/controller/admin/UnitsOfMeasurementController.java
+++ src/main/java/com/pizza/crm/controller/admin/UnitsOfMeasurementController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,17 +20,26 @@ public class UnitsOfMeasurementController {
     }
 
     @RequestMapping("/measurement")
-    public String getAllMeasurement(Model model) {
+    public String getAllUnits(Model model) {
         model.addAttribute("allMeasurement", unitsOfMeasurment.getAll());
         model.addAttribute("AddUnit", new UnitsOfMeasurement());
         return "UnitsOfMeasurement";
     }
 
     @RequestMapping(value = "/measurement/add")
-    public String addUser(@ModelAttribute("AddUnit") UnitsOfMeasurement unit){
+    public String addUnit(@ModelAttribute("AddUnit") UnitsOfMeasurement unit) {
         unitsOfMeasurment.save(unit);
         return "redirect:/measurement";
     }
+
+
+    @RequestMapping(value = "/measurement/delete/{id}")
+    public String deteteUnit(@PathVariable("id") Long id) {
+        unitsOfMeasurment.deleteById(id);
+        return "redirect:/measurement";
+    }
+
+
 
 
 }
