8a9976492f8ea9568f751114457c1431b64dfc17
Vladislav654312 <34103364+Vladislav654312@users.noreply.github.com>
2018-05-16 17:13:09 +0500

    @PostMapping("/admin/discount/getSchedule")

--- src/main/java/com/pizza/crm/controller/admin/rest/DiscountControllerRest.java
+++ src/main/java/com/pizza/crm/controller/admin/rest/DiscountControllerRest.java
@@ -1,7 +1,25 @@
 package com.pizza.crm.controller.admin.rest;
 
+import com.pizza.crm.model.Schedule;
+import com.pizza.crm.service.ScheduleService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * Created by User on 16.05.2018.
+ * Created by User on 12.05.2018.
  */
+@RestController
 public class DiscountControllerRest {
+
+    private final ScheduleService scheduleService;
+
+    public DiscountControllerRest(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+    @PostMapping("/admin/discount/getSchedule")
+    public Schedule getSchedule(@RequestBody Schedule schedule){
+        return scheduleService.getSchedule(schedule.getId());
+    }
 }
