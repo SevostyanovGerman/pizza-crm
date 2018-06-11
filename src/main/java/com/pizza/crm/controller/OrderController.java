28246e543274fade974b048b6d84ea1ab0f17148
Vladislav654312 <34103364+Vladislav654312@users.noreply.github.com>
2018-06-11 21:50:54 +0500

    OrderController

--- src/main/java/com/pizza/crm/controller/OrderController.java
+++ src/main/java/com/pizza/crm/controller/OrderController.java
@@ -2,27 +2,31 @@ package com.pizza.crm.controller;
 
 import com.pizza.crm.model.Employee;
 import com.pizza.crm.model.Order;
+import com.pizza.crm.model.discount.Discount;
 import com.pizza.crm.model.security.User;
-import com.pizza.crm.service.AddedCategoryService;
-import com.pizza.crm.service.EmployeeService;
-import com.pizza.crm.service.OrderService;
+import com.pizza.crm.service.*;
+import com.sun.org.apache.xpath.internal.operations.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class OrderController {
 
-    private final AddedCategoryService categoryService;
     private final EmployeeService employeeService;
     private final OrderService orderService;
+    private final NomenclatureParentGroupService nomenclatureParentGroupService;
 
     @Autowired
-    public OrderController(AddedCategoryService categoryService, EmployeeService employeeService,
-                           OrderService orderService) {
-        this.categoryService = categoryService;
+    public OrderController(EmployeeService employeeService, OrderService orderService,
+                           NomenclatureParentGroupService nomenclatureParentGroupService) {
+        this.nomenclatureParentGroupService = nomenclatureParentGroupService;
         this.employeeService = employeeService;
         this.orderService = orderService;
     }
@@ -33,8 +37,8 @@ public class OrderController {
         Employee employee = employeeService.findByPincode(user.getPincode()).orElseGet(Employee::new);
         String employeeLogin = employee.getLogin();
 
+        model.addAttribute("nomenclatureParentGroups", nomenclatureParentGroupService.findAlNomenclatureParentGroups());
         model.addAttribute("employeeLogin", employeeLogin);
-        model.addAttribute("categories", categoryService.findAllCategories());
 
         Order order = new Order();
         orderService.save(order);
@@ -42,4 +46,5 @@ public class OrderController {
 
         return "order";
     }
+
 }
