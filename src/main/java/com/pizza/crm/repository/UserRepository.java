00dbe0d741a22c3c9d534831a527df2e53cb458f
Денис <denismatveyevaleksandrovich@gmail.com>
2018-06-26 20:55:12 +0400

    Refactoring security packages"

--- src/main/java/com/pizza/crm/repository/UserRepository.java
+++ src/main/java/com/pizza/crm/repository/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
     @Query("FROM User p JOIN FETCH p.roles WHERE p.pincode = (:pincode)")
     User findByPincodeAndFetchRolesEagerly(@Param("pincode") String pincode);
 
